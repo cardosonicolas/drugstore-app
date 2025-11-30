4b068923f171c259131acc8f66864a8394009345
Nicolas <cardoso.leandro.nicolas@gmail.com>
2025-11-30 18:57:30 -0300

    add card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,29 @@
 import Navbar from "@/components/Navbar";
+import Card from "@/components/Card";
+
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 10,
+    image:
+      "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 20,
+    image:
+      "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 30,
+    image:
+      "https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
 
 export default function Home() {
   return (
@@ -7,41 +32,10 @@ export default function Home() {
       <main className="flex w-full max-w-7xl flex-col items-center justify-between py-10 px-4 sm:px-6 lg:px-8 mx-auto">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1>Drugstore App</h1>
-          <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-            <a href="#">
-              <img
-                src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt="Product"
-                className="h-80 w-72 object-cover rounded-t-xl"
-              />
-              <div className="px-4 py-3 w-72">
-                <p className="text-lg font-bold text-black truncate block capitalize">
-                  Product Name
-                </p>
-                <div className="flex items-center">
-                  <p className="text-lg font-semibold text-black cursor-auto my-3">
-                    $149
-                  </p>
-                  <del></del>
-                  <div className="ml-auto">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      className="bi bi-bag-plus"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                      />
-                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </a>
+          <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+            {products.map((product) => (
+              <Card key={product.id} product={product} />
+            ))}
           </div>
         </div>
       </main>
